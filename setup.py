589189683558ade526d6b6fa8a12e9943d72b245
Federico Cunico <federico@cunico.net>
2023-02-14 10:15:48 +0000

    [wip] requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
+import os
 from distutils.core import setup
 from setuptools import find_packages
 
+# lib_folder = os.path.dirname(os.path.realpath(__file__))
+# requirement_path = os.path.join(lib_folder, "requirements.txt")
+# with open(requirement_path, "r") as f:
+#     install_requires = f.readlines()
+
 setup(
-    name='ByteTrackWrapper',
+    name='bytetrack',
     version='0.1',
     packages=find_packages(include=['bytetrack', 'bytetrack.*']),
     install_requires=[
